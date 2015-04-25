f63735ca44761766523d89bd437619535689170b
Ákos Kiszely <akos.kiszely@gmail.com>
2015-04-25 16:48:34 +0200

    Test model hozzáadása

---
+++ models/Test.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "test".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property integer $value
+ */
+class Test extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'test';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['value'], 'integer'],
+            [['name'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'value' => 'Value',
+        ];
+    }
+}
